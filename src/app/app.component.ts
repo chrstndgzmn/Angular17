14e1f0a0c5e39054b0bb5032451c1a3404f09faf
Christian De Guzman <cdeguzman@atrematech.com>
2024-05-17 16:36:43 -0600

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DarkModeService } from './modules/shared/services/dark-mode.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    //components
+    NavbarComponent,
+    CommonModule,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-17';
+  public title = 'angular-17';
+
+  public constructor(public darkModeService: DarkModeService) {}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    title: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    title: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    title: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
